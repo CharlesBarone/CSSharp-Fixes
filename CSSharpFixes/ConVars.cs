12bdac4c868ff77aa0dd33f8db699a18c00c52c6
Charles_ <cbaro4@unh.newhaven.edu>
2024-07-11 21:22:50 -0400

    fix: Disabled ConVars for incomplete fixes.

--- CSSharpFixes/ConVars.cs
+++ CSSharpFixes/ConVars.cs
@@ -28,11 +28,11 @@ public partial class CSSharpFixes
     public FakeConVar<bool> EnableCPhysBoxUseFix = new("css_fixes_cphys_box_use_fix", "Fixes CPhysBox use. Make func_physbox pass itself as the caller in OnPlayerUse.", false);
     //public FakeConVar<bool> EnableNavmeshLookupLagFix = new("css_fixes_navmesh_lookup_lag_fix", "Some maps with built navmeshes would cause tremendous lag.", false); // Commented out since it seems to cause crashes every time I test it...
     public FakeConVar<bool> EnableNoBlock = new("css_fixes_no_block", "Prevent players from blocking each other. (Sets debris collision on every player).", false);
-    public FakeConVar<bool> DisableTeamMessages = new("css_fixes_disable_team_messages", "Disables team join messages.", false);
+    //public FakeConVar<bool> DisableTeamMessages = new("css_fixes_disable_team_messages", "Disables team join messages.", false); //TODO: NOT FINSIHED!
     public FakeConVar<bool> DisableSubTickMovement = new("css_fixes_disable_sub_tick_movement", "Disables sub-tick movement.", false);
     public FakeConVar<bool> EnableMovementUnlocker = new("css_fixes_enable_movement_unlocker", "Enables movement unlocker.", false);
     public FakeConVar<bool> EnforceFullAlltalk = new("css_fixes_enforce_full_alltalk", "Enforces sv_full_alltalk 1.", false);
-    public FakeConVar<bool> EnableEntityStringPurge = new("css_fixes_purge_entity_strings", "Enables purge of the EntityNames stringtable on new rounds", false);
+    //public FakeConVar<bool> EnableEntityStringPurge = new("css_fixes_purge_entity_strings", "Enables purge of the EntityNames stringtable on new rounds", false); //TODO: NOT FINSIHED!
     
     private void RegisterConVars()
     {
@@ -41,11 +41,11 @@ public partial class CSSharpFixes
         EnableCPhysBoxUseFix.ValueChanged += (sender, value) => { _configuration.EnableCPhysBoxUseFix = value; };
         //EnableNavmeshLookupLagFix.ValueChanged += (sender, value) => { _configuration.EnableNavmeshLookupLagFix = value; }; // Commented out since it seems to cause crashes every time I test it...
         EnableNoBlock.ValueChanged += (sender, value) => { _configuration.EnableNoBlock = value; };
-        DisableTeamMessages.ValueChanged += (sender, value) => { _configuration.DisableTeamMessages = value; };
+        //DisableTeamMessages.ValueChanged += (sender, value) => { _configuration.DisableTeamMessages = value; }; //TODO: NOT FINSIHED!
         DisableSubTickMovement.ValueChanged += (sender, value) => { _configuration.DisableSubTickMovement = value; };
         EnableMovementUnlocker.ValueChanged += (sender, value) => { _configuration.EnableMovementUnlocker = value; };
         EnforceFullAlltalk.ValueChanged += (sender, value) => { _configuration.EnforceFullAlltalk = value; };
-        EnableEntityStringPurge.ValueChanged += (sender, value) => { _configuration.EnableEntityStringPurge = value; };
+        //EnableEntityStringPurge.ValueChanged += (sender, value) => { _configuration.EnableEntityStringPurge = value; }; //TODO: NOT FINSIHED!
         
         RegisterFakeConVars(typeof(ConVar));
     }
